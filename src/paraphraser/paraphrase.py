0a568f8902e545fb73243f72ca91cf11ec0d04de
Alankrit <amishra1@lakeheadu.ca>
2022-08-03 11:05:24 -0400

    Added parameter settings to parrot.augment

--- src/paraphraser/paraphrase.py
+++ src/paraphraser/paraphrase.py
@@ -1,12 +1,21 @@
 from paraphraser.parrot import Parrot
-    
+
+
 class Parapharser:
     def paraphrase(self, model, sentence):
         parrot = Parrot(model)
-        para_phrases = parrot.augment(input_phrase=sentence, use_gpu=False)
+        para_phrases = parrot.augment(input_phrase=sentence,
+                                      use_gpu=False,
+                                      diversity_ranker="levenshtein",
+                                      do_diverse=False,
+                                      max_return_phrases=10,
+                                      max_length=32,
+                                      adequacy_threshold=0.23,
+                                      fluency_threshold=0.75)
         return para_phrases
 
     def paraphrase_batch(self, model, sentences):
         parrot = Parrot(model)
-        para_phrases = parrot.augment_batch(input_phrase=sentences, use_gpu=False)
+        para_phrases = parrot.augment_batch(
+            input_phrase=sentences, use_gpu=False)
         return para_phrases
