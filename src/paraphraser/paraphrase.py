210e9f0a6fbb54520738a12825b3a0906b53254a
Alankrit <amishra1@lakeheadu.ca>
2022-08-04 11:26:28 -0400

    Added grammer correction model

--- src/paraphraser/paraphrase.py
+++ src/paraphraser/paraphrase.py
@@ -2,6 +2,11 @@ from paraphraser.parrot import Parrot
 
 
 class Parapharser:
+    def correct_grammar(self, model, sentence):
+        happy_tt, beam_settings = model.get_grammar_model()
+        correct_sen = happy_tt.generate_text(sentence, args=beam_settings).text
+        return correct_sen
+
     def paraphrase(self, model, sentence):
         parrot = Parrot(model)
         para_phrases = parrot.augment(input_phrase=sentence,
@@ -12,6 +17,9 @@ class Parapharser:
                                       max_length=32,
                                       adequacy_threshold=0.23,
                                       fluency_threshold=0.75)
+
+        para_phrases = [[self.correct_grammar(model=model, sentence=para_phrase[0]), para_phrase[1]]
+                        for para_phrase in para_phrases]
         return para_phrases
 
     def paraphrase_batch(self, model, sentences):
